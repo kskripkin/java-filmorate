433f401a8eb13552c6a1b9774ddc65ba54f21e14
kskripkin <k.skripkin@internet.ru>
2022-09-27 21:19:12 +0300

    Спринт 10 Исправлены тесты

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,14 +2,11 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage.FilmStorage;
 
 @SpringBootApplication
 public class FilmorateApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(FilmorateApplication.class, args);
-		FilmStorage filmStorage = context.getBean(FilmStorage.class);
+		SpringApplication.run(FilmorateApplication.class, args);
 	}
 }
